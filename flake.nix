954c658b870c81019c8f64d2081ce6c2aa85ddb8
Jean-Francois Labonte <jefflabonte@protonmail.com>
2024-09-20 22:50:07 -0400

    Run nixfmt on flake.nix

--- flake.nix
+++ flake.nix
@@ -4,19 +4,23 @@
   # Nixpkgs / NixOS version to use.
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
 
       # to work with older version of flakes
-      lastModifiedDate =
-        self.lastModifiedDate or self.lastModified or "19700101";
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
 
       # Generate a user-friendly version number.
       version = builtins.substring 0 8 lastModifiedDate;
 
       # System types to support.
-      supportedSystems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
@@ -24,12 +28,16 @@
       # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 
-    in {
+    in
+    {
 
       # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
           go-hello = pkgs.buildGo123Module {
             pname = "go-hello";
             inherit version;
@@ -52,26 +60,39 @@
           container = pkgs.dockerTools.buildImage {
             name = "go-hello";
             tag = "latest";
-            copyToRoot = [ self.packages.${system}.go-hello pkgs.cacert ];
+            copyToRoot = [
+              self.packages.${system}.go-hello
+              pkgs.cacert
+            ];
             config = {
               ExposedPorts = {
                 "8080/tcp" = { };
               };
-              Entrypoint =
-                [ "${self.packages.${system}.go-hello}/bin/go-hello" ];
+              Entrypoint = [ "${self.packages.${system}.go-hello}/bin/go-hello" ];
               Cmd = [ ];
             };
+            diskSize = 1024;
           };
-        });
+        }
+      );
 
       # Add dependencies that are only needed for development
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
           default = pkgs.mkShell {
-            buildInputs = with pkgs; [ go gopls gotools go-tools ];
+            buildInputs = with pkgs; [
+              go
+              gopls
+              gotools
+              go-tools
+            ];
           };
-        });
+        }
+      );
 
       # The default package for 'nix build'. This makes sense if the
       # flake provides only one package or there is a clear "main"
